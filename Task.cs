99042c888a58b4787996a56aa1b9e52fda232b7f
Togrul Talibov <togrultalibov@Togrul-MacBook-Pro.local>
2022-02-07 11:34:46 +0400

    first commit

---
+++ Task.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace praktika.js
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sozu daxil edin");
+
+            string a;
+
+            a = Console.ReadLine();
+
+            Console.WriteLine("Herif daxil edin");
+
+
+
+            char b;
+
+            char.TryParse(Console.ReadLine(), out b);
+
+            FindCharIndex(a, b);
+
+
+
+        }
+
+        static void FindCharIndex(string soz, char herif)
+        {
+            //alma  a
+            for (int i = 0; i < soz.Length; i++)
+            {
+
+                if (soz[i] == herif)
+                {
+
+                    Console.WriteLine(i);
+
+                }
+                else
+                {
+                    int g = -1;
+                    Console.WriteLine(g);
+                    break;
+                }
+
+            }
+
+
+        }
+    }
+
+}
---
+++ Task2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace praktika.js
+{
+	 class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int.TryParse(Console.ReadLine(), out a);
+            IsPrimeMath(a); 
+        
+        }
+        static void IsPrimeMath(int a)
+        {
+            if (a == 2)
+            {
+                Console.WriteLine("Eded Sadedir");
+            }
+            
+            for (int i = 2; i < a; i++)
+            {
+                
+                if(a % i == 0)
+                {
+                    Console.WriteLine("Eded Murkebdir");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Eded Sadedir");
+                    break;
+                }
+                
+
+            }
+
+        }
+
+
+
+
+    }
+
+}
